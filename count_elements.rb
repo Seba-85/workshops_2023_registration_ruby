8f5cd7745fcf85fa7088667fe6234855da785535
Seba-85 <sebastianlubanski.eu@gmail.com>
2023-05-02 21:36:18 +0200

    Add new metodhs to CountElements and ValidatePassword classes

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,17 @@
-class CountElements
+# frozen_string_literal: true
 
+class CountElements
+  def initialize(array)
+    @array = array
+  end
+
+  def perform
+    counts = Hash.new(0)
+
+    @array.each do |element|
+      counts[element] += 1
+    end
+
+    counts
+  end
 end
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,43 @@
-class ValidatePassword
+# frozen_string_literal: true
 
+class ValidatePassword
+  def initialize(password)
+    @password = password
+  end
+
+  def perform
+    return false unless characters_in_range?
+
+    return false unless uppercase_and_lowercase? && contains_numbers?
+
+    return false unless contains_special_characters?
+
+    return false if contains_repeating_characters?
+
+    true
+  end
+
+  private
+
+  attr_reader :password
+
+  def characters_in_range?
+    password.length.between?(6, 24)
+  end
+
+  def uppercase_and_lowercase?
+    password.match?(/[A-Z]/) && password.match?(/[a-z]/)
+  end
+
+  def contains_numbers?
+    password.match?(/\d/)
+  end
+
+  def contains_special_characters?
+    password.match?(/[!@#$%&*+=:;?<>\[\]]/)
+  end
+
+  def contains_repeating_characters?
+    password.match?(/(.)\1{2,}/)
+  end
 end
